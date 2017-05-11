ae12ec5e88520ac5f59b1c047e855cb0a076a7e2
Marlon Dias <marlondias@outlook.com>
2017-05-10 22:41:21 -0300

    String challenges ok

--- textFun.js
+++ textFun.js
@@ -32,7 +32,7 @@ function piglatin(txt){
 }
 
 function vowelCounter(txt){
-	const original = txt.split("");
+	const original = txt.toLowerCase().split("");
 	const unicodeA = ["a", "à", "á", "ã", "â"];
 	const unicodeE = ["e", "è", "é", "ẽ", "ê"];
 	const unicodeI = ["i", "ì", "í", "ĩ", "î"];
@@ -41,7 +41,6 @@ function vowelCounter(txt){
 	const counter = {a: 0, e: 0, i: 0, o: 0, u: 0};
 
 	for(let char of original){
-		char = char.toLowerCase();
 		if(unicodeA.indexOf(char) !== -1) counter.a++;
 		else if(unicodeE.indexOf(char) !== -1) counter.e++;
 		else if(unicodeI.indexOf(char) !== -1) counter.i++;
@@ -50,6 +49,39 @@ function vowelCounter(txt){
 	}
 
 	return counter;
+}
+
+function palindrome(txt){
+	if(txt.length === 0) return false;
+	else if(txt.length < 2) return true;
+
+	const original = txt.toLowerCase().split("");
+
+	for(let i=0; i < original.length/2; i++){
+		if(original[i] !== original[original.length - 1 - i]) return false;
+	}
+	return true;
+}
+
+function wordCounter(txt){
+	const validConsonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+	const validVowels = ["a", "à", "á", "ã", "â", "e", "è", "é", "ẽ", "ê", "i", "ì", "í", "ĩ", "î", "o", "ò", "ó", "õ", "ô", "u", "ù", "ú", "ũ", "û"];
+	const original = txt.toLowerCase().split("");
+	let words = 0, readingWord = false;
+
+	for(char of original){
+		if(!readingWord && (validConsonants.indexOf(char) !== -1 || validVowels.indexOf(char) !== -1)){
+			//Encontrou uma palavra e deve mudar de estado
+			words++;
+			readingWord = !readingWord;
+		}
+		else if(readingWord && (validConsonants.indexOf(char) === -1 && validVowels.indexOf(char) === -1)){
+			//Encontrou um separador e deve mudar de estado
+			readingWord = !readingWord;
+		}
+	}
+
+	return words;
 }
 
 function main(){
@@ -63,6 +95,8 @@ function main(){
 			if (arg.toLowerCase() === "--reverse") options.add("reverse");
 			if (arg.toLowerCase() === "--piglatin") options.add("piglatin");
 			if (arg.toLowerCase() === "--vowels") options.add("vowels");
+			if (arg.toLowerCase() === "--palindrome") options.add("palindrome");
+			if (arg.toLowerCase() === "--words") options.add("words");
 		}
 
 		if(options.size === 0){
@@ -75,8 +109,13 @@ function main(){
 			if (options.has("piglatin")) console.log("Pig Latin: " + piglatin(targetString));
 			if (options.has("vowels")){
 				const vows = vowelCounter(targetString);
-				console.log(`Vogais: A = ${vows.a}, E = ${vows.e}, I = ${vows.i}, O = ${vows.o}, U = ${vows.u}`);
+				console.log(`Vowels: A = ${vows.a}, E = ${vows.e}, I = ${vows.i}, O = ${vows.o}, U = ${vows.u}`);
 			}
+			if(options.has("palindrome")){
+				if(palindrome(targetString)) console.log("Palindrome: Yes");
+				else console.log("Palindrome: No");
+			}
+			if (options.has("words")) console.log("Words: " + wordCounter(targetString));
 		}
 
 	}
