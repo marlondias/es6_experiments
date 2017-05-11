3e12356103206caf14465e1a8b71b5ea0e14695b
Marlon Dias <marlondias@outlook.com>
2017-05-11 15:22:09 -0300

    Ceasar's cipher working

--- ciphers.js
+++ ciphers.js
@@ -10,6 +10,7 @@ function showHelp(){
 
 function ceasar(txt, offset){
 	if(offset === 0) return txt;
+	if(offset >= 26 || offset <= -26) offset = offset % 26; //Maybe adapt for different alphabets
 
 	const unicodeA = ["a", "à", "á", "ã", "â"];
 	const unicodeE = ["e", "è", "é", "ẽ", "ê"];
@@ -18,20 +19,20 @@ function ceasar(txt, offset){
 	const unicodeU = ["u", "ù", "ú", "ũ", "û"];
 	const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 	const original = txt.toLowerCase().split("");
-	const result = "";
+	let result = "";
 
 	for(let char of original){
-		if(unicodeA.indexOf(char) === -1) char = "a";
-		else if(unicodeE.indexOf(char) === -1) char = "e";
-		else if(unicodeI.indexOf(char) === -1) char = "i";
-		else if(unicodeO.indexOf(char) === -1) char = "o";
-		else if(unicodeU.indexOf(char) === -1) char = "u";
+		if(unicodeA.indexOf(char) !== -1) char = "a";
+		else if(unicodeE.indexOf(char) !== -1) char = "e";
+		else if(unicodeI.indexOf(char) !== -1) char = "i";
+		else if(unicodeO.indexOf(char) !== -1) char = "o";
+		else if(unicodeU.indexOf(char) !== -1) char = "u";
 
-		let position = alphabet.indexOf(char);
-		if (position === -1) result += "?";
+		let pos = alphabet.indexOf(char);
+		if (pos === -1) result += char;
 		else{
-			position -= offset;
-			result += (position >= 0) ? alphabet[position] : alphabet[alphabet.length-position];
+			pos -= offset;
+			result += (pos < 0) ? alphabet[alphabet.length + pos] : alphabet[pos];
 		}
 	}
 
@@ -43,13 +44,36 @@ function main(){
 
 	if (relevantArgs.length > 1){
 		const targetString = relevantArgs.pop(); // Last argument is ALWAYS the string
-		const options = new Set();
+		const options = new Map();
+
+		for(let arg of relevantArgs){
+			arg = arg.toLowerCase().split("=");
+			if(arg.length === 2){
+				if(arg[0] === "--ceasarencode"){
+					const value = arg[1] * 1;
+					if(!isNaN(value)) options.set("ceasarEnc", value);
+				}
+				else if(arg[0] === "--ceasardecode"){
+					const value = arg[1] * -1;
+					if(!isNaN(value)) options.set("ceasarDec", value);
+				}
+			}
+		}
 
 		if(options.size === 0){
 			console.log(`No valid arguments found! The string is: ${targetString}`);
 		}
 		else{
 			console.log("Original: " + targetString);
+
+			if(options.has("ceasarEnc")){
+				const offset = options.get("ceasarEnc");
+				console.log(`Ceasar's Cipher encoded by ${offset} is: ` + ceasar(targetString, offset));
+			}
+			if(options.has("ceasarDec")){
+				const offset = options.get("ceasarDec");
+				console.log(`Ceasar's Cipher decoded by ${offset} is: ` + ceasar(targetString, offset));
+			}
 		}
 	}
 	else{
